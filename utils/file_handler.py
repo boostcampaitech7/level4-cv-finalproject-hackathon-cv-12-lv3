568c5fbcdccecebaf4f658516c054ee6db89257a
lkl4502 <lkl4502@naver.com>
2025-01-22 07:21:14 +0000

    [#20] ✨ Add file_handler extra function

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -1,4 +1,49 @@
 import json
+import cv2
+import torch
+import numpy as np
+
+from PIL import Image
+
+
+def save_image(image, path):
+    if isinstance(image, np.ndarray):
+        image = Image.fromarray(image)
+    image.save(path)
+    # TODO PIL or Ndarray 아닐 때 TypeError 처리 해야됨.
+
+
+def save_json(data, path):
+    # JSON 파일로 저장
+    with open(path, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, indent=4, ensure_ascii=False)
+        print(f"{path} 경로에 json 파일 저장!")
+
+
+def save_text(text, path):
+    with open(path, 'w') as f:
+        f.write(text)
+
+
+def save_image(image, path):
+    if isinstance(image, np.ndarray):
+        cv2.imwrite(path, image)
+    elif isinstance(image, Image.Image):
+        image.save(path)
+    else:
+        raise TypeError(
+            f"Invalid Input Type : Expected an instance of Image.Image or np.ndarray, but received{type(image)}")
+
+
+def select_device(device):
+    if device is not None:
+        return device
+
+    device = 'cpu'
+    if torch.cuda.is_available():
+        device = 'cuda'
+    return device
+
 
 def load_json_file(file_path):
     try:
