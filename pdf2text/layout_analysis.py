5a473ba87c715e935d0b6612ca52b3aa96f2a3b3
lkl4502 <lkl4502@naver.com>
2025-01-22 09:19:35 +0000

    [#15] ✨ Add Naver Cloud Donut Model

--- pdf2text/layout_analysis.py
+++ pdf2text/layout_analysis.py
@@ -33,8 +33,35 @@ class LayoutAnalyzer:
     def __init__(self,
                  device: str = None):
         # TODO : Layout Analyzer Class
+        self.device = select_device(device)
+        self.processor = DonutProcessor.from_pretrained(
+            "naver-clova-ix/donut-base-finetuned-cord-v2")
+        self.model = VisionEncoderDecoderModel.from_pretrained(
+            "naver-clova-ix/donut-base-finetuned-cord-v2").to(self.device)
         pass
 
-    def parse(self):
+    def parse(self, image):
         # TODO : Layout Analysis process
-        pass
+        task_prompt = "<s_cord-v2>"
+        decoder_input_ids = self.processor.tokenizer(
+            task_prompt, add_special_tokens=False, return_tensors="pt").input_ids
+
+        pixel_values = self.processor(image, return_tensors="pt").pixel_values
+
+        outputs = self.model.generate(
+            pixel_values.to(self.device),
+            decoder_input_ids=decoder_input_ids.to(self.device),
+            max_length=self.model.decoder.config.max_position_embeddings,
+            pad_token_id=self.processor.tokenizer.pad_token_id,
+            eos_token_id=self.processor.tokenizer.eos_token_id,
+            use_cache=True,
+            bad_words_ids=[[self.processor.tokenizer.unk_token_id]],
+            return_dict_in_generate=True,
+        )
+
+        sequence = self.processor.batch_decode(outputs.sequences)[0]
+        sequence = sequence.replace(self.processor.tokenizer.eos_token, "").replace(
+            self.processor.tokenizer.pad_token, "")
+        # remove first task start token
+        sequence = re.sub(r"<.*?>", "", sequence, count=1).strip()
+        return self.processor.token2json(sequence)
