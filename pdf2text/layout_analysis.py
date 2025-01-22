bee6dc3c136080401d50686299e8f73243fc1221
lkl4502 <lkl4502@naver.com>
2025-01-22 09:16:32 +0000

    [#15] ✨ Add Library and ElementType Class

--- pdf2text/layout_analysis.py
+++ pdf2text/layout_analysis.py
@@ -1,6 +1,37 @@
+import re
+import torch
+
+from enum import Enum
+from utils import select_device
+from transformers import AutoModelForTokenClassification, AutoProcessor
+from transformers import AutoModel
+from transformers import DonutProcessor, VisionEncoderDecoderModel
+import cv2
+import numpy as np
+from doclayout_yolo import YOLOv10
+
+
+class ElementType(Enum):
+    ABANDONED = -2  # 텍스트 추출이 필요없는 부분
+    IGNORED = -1  # 무시해야할 영역
+    UNKNOWN = 0  # 아직 결정되지 않은 영역
+    TEXT = 1
+    TITLE = 2
+    FIGURE = 3
+    TABLE = 4
+    FORMULA = 5
+    PLAIN_TEXT = 11  # 순수 텍스트 영역
+
+    def __repr__(self) -> str:
+        return self.name
+
+    def __str__(self) -> str:
+        return self.name
+
 
 class LayoutAnalyzer:
-    def __init__(self):
+    def __init__(self,
+                 device: str = None):
         # TODO : Layout Analyzer Class
         pass
 
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
-from .pdf_to_ocr import pdf_to_image, images_to_text
+from .pdf_to_ocr import pdf_to_image, images_to_text, select_device
 from .text_preprocessing import clean_text
 from .text_chunking import chunkify_to_num_token
 from .chatbot import query_and_respond
@@ -6,4 +6,4 @@ from .multichat import MultiChatManager
 from .query_refinement import llm_refine
 
 __all__ = ['pdf_to_image', 'images_to_text', 'clean_text', 'chunkify_to_num_token', 'query_and_respond',
-           'MultiChatManager', 'llm_refine']
+           'MultiChatManager', 'llm_refine', 'select_device']
