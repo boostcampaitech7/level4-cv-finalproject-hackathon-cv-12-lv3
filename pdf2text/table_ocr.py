cca8c2534db0fd6b0513db9db54cafd3b7c77299
lkl4502 <lkl4502@naver.com>
2025-01-22 06:55:19 +0000

    [#20] ✨ Create pix2text folder and structure

---
+++ pdf2text/table_ocr.py
@@ -0,0 +1,9 @@
+
+class TalbeOCR:
+    def __init__(self):
+        # TODO
+        pass
+
+    def ocr(self):
+        # TODO table ocr
+        return
---
+++ pdf2text/text_ocr.py
@@ -0,0 +1,9 @@
+
+class TextOCR:
+    def __init__(self):
+        # TODO
+        pass
+
+    def ocr(self):
+        # TODO: Text OCR 구현
+        return "추출 텍스트"
