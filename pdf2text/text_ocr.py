a941052972d7e3eff380b7272f0a20a8031669b9
oweixx <lowzxx@naver.com>
2025-01-23 11:23:04 +0900

    [#17] ✨ Modify TextOCR

--- pdf2text/text_ocr.py
+++ pdf2text/text_ocr.py
@@ -13,23 +13,3 @@ class TextOCR :
         result = self.ocr_engine.ocr(img, det=True, rec=True, cls=False)
 
         return result
-
-    # def recognize_only(self, img: np.ndarray, **kwargs):
-    #     """
-    #     감지된 텍스트 영역에서 텍스트만 인식.
-    #     """
-    #     result = self.ocr_engine.ocr(img, det=False, rec = True, cls=False)
-    #     if not result or not result[0]:  # None 또는 빈 결과 처리
-    #         return {'text': '', 'score': 0.0}
-
-    #     recognized_texts = []
-    #     confidences = []
-    #     for region in result :
-    #         text, confidence = region[0]
-    #         recognized_texts.append(text)
-    #         confidences.append(confidence)
-
-    #     combined_text = " ".join(recognized_texts)
-    #     avg_confidence = sum(confidences) / len(confidences) if confidences else 0.0
-
-    #     return {'text': combined_text, 'score': avg_confidence}
