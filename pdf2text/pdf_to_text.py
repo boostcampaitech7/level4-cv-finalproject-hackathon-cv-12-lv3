0b3993e830523c76fafeabcc9f77fd37864e71b7
lkl4502 <lkl4502@naver.com>
2025-01-25 19:16:54 +0900

    [#18] ✨ Add branching logic based on element type in layout parsing

--- pdf2text/pdf_to_text.py
+++ pdf2text/pdf_to_text.py
@@ -34,3 +34,30 @@ class Pdf2Text(object):
         layout_output, col_meta = self.layout_analysis.parse(img)
 
         # NOTE fitz에서 제공하는 page_id or page_number를 추가?
+
+        final_outputs = []
+        for layout_ele in layout_output:
+            ele_type = layout_ele['type']
+            if ele_type == ElementType.IGNORED:
+                continue
+
+            bbox = box2list(layout_ele['position'])
+            crop_img = img.crop(bbox)
+
+            if ele_type in (ElementType.TEXT, ElementType.TITLE, ElementType.PLAIN_TEXT):
+                # TODO TEXT OCR 이전의 전처리 코드 작성
+                pass
+            elif ele_type == ElementType.FORMULA:
+                # TODO FORMULA OCR 이전의 전처리 코드 작성
+                # 여기로는 독립된 FORMULA만 입력으로 들어온다.
+                pass
+            elif ele_type == ElementType.TABLE:
+                # TODO TABLE OCR 이전의 전처리 코드 작성
+                pass
+            elif ele_type == ElementType.FIGURE:
+                # NOTE 이미지의 경우에는 어떤 방식으로 처리할지 결정되면 진행
+                pass
+            else:  # 나머지 타입은 처리하지않는 유형이므로 무시
+                pass
+
+        return final_outputs
