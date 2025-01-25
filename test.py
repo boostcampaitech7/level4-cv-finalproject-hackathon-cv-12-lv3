5bc81f58b9a26ac9193dbb59b8dd227fc1db380d
oweixx <lowzxx@naver.com>
2025-01-25 17:44:51 +0900

    [#40]✨ Remove test file

--- test.py
+++
@@ -1,7 +0,0 @@
-from pdf2text.text_pipeline import Text_Extractor
-import cv2
-img = cv2.imread('test.jpg')
-model = Text_Extractor(lang = 'korean')
-result = model.Recognize(img)
-outs = [text for box, text in result]
-print('변형: ' + ' '.join(outs))
